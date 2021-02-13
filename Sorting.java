dd3240c3d580327a0f9ce945364ab34d3b922d28
EstebanAG1005 <60299338+EstebanAG1005@users.noreply.github.com>
2021-02-13 16:01:27 -0600

    Update Sorting.java

--- Sorting.java
+++ Sorting.java
@@ -122,45 +122,60 @@ public class Sorting implements Comparable<T>{
   *@param izq
   *@param der
   */
- public static void quicksort(int A[], int izq, int der) {
-  // tomamos primer elemento como pivote
-  int pivote=A[izq]; 
-  // i realiza la búsqueda de izquierda a derecha
-  int i=izq;
-  // j realiza la búsqueda de derecha a izquierda        
-  int j=der;         
-  int aux;
- 
- // mientras no se crucen las búsquedas
-  while(i < j){     
-     // busca elemento mayor que pivote                     
-     while(A[i] <= pivote && i < j) i++;
-     // busca elemento menor que pivote
-     while(A[j] > pivote) j--;   
-     //si no se han cruzado        
-     if (i < j) { 
-         // los intercambia
-         aux= A[i];                    
-         A[i]=A[j];
-         A[j]=aux;
-     }
-   }
-   
-   // se coloca el pivote en su lugar de forma que tendremos
-   A[izq]=A[j];   
-    // los menores a su izquierda y los mayores a su derecha   
-   A[j]=pivote;     
-   
-   if(izq < j-1)
-      // ordenamos subarray izquierdo
-      quicksort(A,izq,j-1);         
-   if(j+1 < der)
-      // ordenamos subarray derecho
-      quicksort(A,j+1,der);          
-   
-}
-
-
+ int partition(int arr[], int low, int high) 
+    { 
+        int pivot = arr[high];  
+        int i = (low-1); // index of smaller element 
+        for (int j=low; j<high; j++) 
+        { 
+            // If current element is smaller than the pivot 
+            if (arr[j] < pivot) 
+            { 
+                i++; 
+  
+                // swap arr[i] and arr[j] 
+                int temp = arr[i]; 
+                arr[i] = arr[j]; 
+                arr[j] = temp; 
+            } 
+        } 
+  
+        // swap arr[i+1] and arr[high] (or pivot) 
+        int temp = arr[i+1]; 
+        arr[i+1] = arr[high]; 
+        arr[high] = temp; 
+  
+        return i+1; 
+    } 
+  
+  
+    /* The main function that implements QuickSort() 
+      arr[] --> Array to be sorted, 
+      low  --> Starting index, 
+      high  --> Ending index */
+    void sort(int arr[], int low, int high) 
+    { 
+        if (low < high) 
+        { 
+            /* pi is partitioning index, arr[pi] is  
+              now at right place */
+            int pi = partition(arr, low, high); 
+  
+            // Recursively sort elements before 
+            // partition and after partition 
+            sort(arr, low, pi-1); 
+            sort(arr, pi+1, high); 
+        } 
+    } 
+  
+    /* A utility function to print array of size n */
+    static void printArray(int arr[]) 
+    { 
+        int n = arr.length; 
+        for (int i=0; i<n; ++i) 
+            System.out.print(arr[i]+" "); 
+        System.out.println(); 
+    } 
   /**
   * Funcion que se utiliza para obtener el valor mas grande de un array
   *@param arr[] Array del que se quiere sacar el valor maximo
